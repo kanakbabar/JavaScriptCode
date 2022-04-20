2ff9a9b803d757de9044df6c674c9b82d6158e1f
kanakbabar <kanakbabar999@gamil.com>
2022-04-20 14:04:40 +0530

    DataType

---
+++ DataType.js
@@ -0,0 +1,48 @@
+// Data type =>  
+// js is dynamic type language means you don not need to specified. 
+
+
+//  1] primitive datatype  - string , number , undefined , null , symbol  , boolean , bigint.   
+// primitive is data is not object and no method.
+
+
+//  2] Non primitive datatype - objects , array ,  regex(regular expression).
+//  They cannot store the value of a variable directly in memory. They store a memory address of the variable
+
+// string -
+var x= "kanak"; // it is also declare in single code.
+console.log(x);
+
+// number -
+a=10;
+console.log(a);
+
+// boolean -
+var y=true;
+console.log(y);
+
+// bigint -
+var s =145266875694126n;
+console.log(s);
+
+// undefined -
+let a;
+console.log(a);
+
+
+// null -
+var z=null;
+console.log(z);
+
+// symbol -
+let x = Symbol('kanak');
+console.log(x);
+
+// object -
+const x= {fname:'kanak',lname:'babar'};
+console.log(x);
+
+
+   
+
+   
