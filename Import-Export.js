e10e5dbb99bd958f2a4b6334d521da08cefc80c0
kanakbabar <kanakbabar999@gamil.com>
2022-04-20 14:19:45 +0530

    Import Export

---
+++ Import-Export.js
@@ -0,0 +1,26 @@
+/*function fun()
+    {
+        console.log('function first');
+    }
+    function fun1()
+    {
+        console.log('function second');
+    }
+
+    module.export={fun,fun1};
+
+    function m()
+    {
+        return 'kanak';
+    }
+    module.export=m;*/
+
+
+   export default function say(){console.log('hi');}
+
+/*function sum(name)
+{
+   let name ='kanak';
+   return name;
+}
+export {sum};*/
