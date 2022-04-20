43101d612bdefe82ae8e3fc01eab5a4edae93934
kanakbabar <kanakbabar999@gamil.com>
2022-04-20 14:17:35 +0530

    Immegiately Invoke Function Expression

---
+++ IIFE.js
@@ -0,0 +1,7 @@
+//  IIFE => (immegiately invike function expression)
+//it is run as soon as it is defined.
+// why we use - they not pollute the global object
+
+(()=>{console.log('hello world');})();
+
+
