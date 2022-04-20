24b06471400b95bf0dc95a42aa8d761d7dee9cf7
kanakbabar <kanakbabar999@gamil.com>
2022-04-20 14:30:11 +0530

    operators

---
+++ Operators.js
@@ -0,0 +1,54 @@
+// operators in js =>
+
+// 1] unary operators - it has single operand(x=-x)
+var x=10;
+var x=-x;
+console.log(x);
+
+
+// 2] binary operators - it has two operand(x=x+5)
+var y=5+6;
+console.log(y);
+
+//operand - 3 + 4  3and 4 is operand + is operators
+
+// java script operators =>
+//1] Arithmatic operators -
+a=10;
+b=10;
+console.log('addition :',(a+b));
+console.log('subtraction :',(a-b));
+console.log('multiplications :',(a*b));
+console.log('division :',(a/b));
+console.log('exponentiation :',(a**b));
+console.log('modulus :',(a%b));
+console.log('incrementation :',++a);
+console.log('decrementation :',--a);
+console.log('incrementation :',a++);
+console.log('decrementation :',a--);
+
+// 2] Assignment operators -
+x=10;
+y=20;
+console.log('= :',x=y);
+console.log('+= :',x+=y);
+console.log('-= :',x-=y);
+console.log('*= :',x*=y);
+console.log('/= :',x/=y);
+console.log('%= :',x%=y);
+console.log('**= :',x**=y);
+
+// 3] Comparison operators -
+j=10;
+k=10;
+console.log('== :',(j==k));
+console.log('!= :',(j!=k));
+console.log('<= :',(j<=k));
+console.log('>= :',(j>=k));
+console.log('< :',(j<k));
+console.log('> :',(j>k));
+
+// 4] Logical operators -   &&  , ||  ,!
+// 5] Type of operators -  typeof  => Returns the type of a variable   ,   instanceof => Returns true if an object is an instance of an object type
+// 6] Bitwise operators - & , | , ~ , ^ , << , >> , <<<.                          
+ 
