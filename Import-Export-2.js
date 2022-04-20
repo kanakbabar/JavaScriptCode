44c44d827e3ac588df41b113f3eaa8c6791d0908
kanakbabar <kanakbabar999@gamil.com>
2022-04-20 14:57:02 +0530

    Import Export - 2

---
+++ Import-Export-2.js
@@ -0,0 +1,12 @@
+/*const {fun,fun1}=require ('./Import-Export');
+console.log(fun);
+fun1();*/
+
+/*const  user = require('./Import-Export')
+console.log(user);*/
+
+
+import speak from "./Import-Export.js";
+speak();
+//import {sum} from "./Import-Export.js";
+//console.log(sum);
