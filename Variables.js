5ac1a6abb065ab8fbd1389346474a3fce2b4d58e
kanakbabar <kanakbabar999@gamil.com>
2022-04-20 14:49:35 +0530

    Variables

---
+++ Variables.js
@@ -0,0 +1,19 @@
+//  variables it is just way to declare a value or initialize value.
+// There are four way to declare a variables.
+
+// 1] using var -
+var a=10;
+console.log(a);
+
+// 2] using let -
+let b=20;
+console.log(b);
+
+// 3] using const -
+// we use const after we can not change the value of variables.
+const c=30;
+console.log(c);
+
+// 4] using nothing -
+d=40;
+console.log(d);
